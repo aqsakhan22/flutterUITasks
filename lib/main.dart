d0360f26afdca3066d8ec11cc021641df812f3be
aqsakhan22 <toheed.wajid@bmacapital.com>
2024-10-15 12:17:43 +0500

    15 oct

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutteruitask/QR_examples/GenerateQRCode.dart';
 import 'package:flutteruitask/QR_examples/Qr_main.dart';
 import 'package:flutteruitask/QR_examples/qr_example1.dart';
 import 'package:flutteruitask/QR_examples/qr_example2.dart';
 import 'package:flutteruitask/carousel/example.dart';
+import 'package:flutteruitask/interview_questions_example/extension_example.dart';
 import 'package:flutteruitask/listview_example/listView_example.dart';
 import 'package:flutteruitask/parallex_examples/paralex_Ex.dart';
 import 'package:flutteruitask/persistant_shopping_Cart/persistent_shopping_cart.dart';
 import 'package:flutteruitask/scroll_animate/custom_animation_ex.dart';
 import 'package:flutteruitask/scroll_animate/fade_Example.dart';
 import 'package:flutteruitask/scroll_animate/rotate_example.dart';
+import 'package:flutteruitask/scroll_animate/scrolling_fab_animaton.dart';
 import 'package:flutteruitask/scroll_animate/slide_example.dart';
 import 'package:flutteruitask/scroll_animate/text_example.dart';
 import 'package:persistent_shopping_cart/persistent_shopping_cart.dart';
-
+import 'dart:convert';
+import 'dart:io';
 Future<void> main() async{
  // await PersistentShoppingCart().init();
+ //  final jsonData = await Isolate.run(_readAndParseJson);
   runApp(const MyApp());
 }
 
+Future<Map<String, dynamic>> _readAndParseJson() async {
+  final fileData = await File("filename").readAsString();
+  final jsonData = jsonDecode(fileData) as Map<String, dynamic>;
+  return jsonData;
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -32,7 +43,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:QRCodePlayground()
+      home:
+      //ExtensionExample()
+      //QRCodePlayground()
       //QRMain()
       //GenerateQRCode()
       //QRScreen()
@@ -41,7 +54,8 @@ class MyApp extends StatelessWidget {
       //TextExamples()
       //SlideExamples()
       //RotateExamples()
-      //FadeExamples()
+     // FadeExamples(),
+      ScrollAnimationFab()
       //CustomAnimationExample()
       //Example()
       //ProductsScreen(),
